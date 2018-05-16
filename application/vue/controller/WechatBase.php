f403ce181448cbac5afef1a550136cb1ba54bec1
[macccha520] <[1121787590@qq.com]>
2018-05-16 22:18:30 +0800

    update 22/18

--- application/vue/controller/WechatBase.php
+++ application/vue/controller/WechatBase.php
@@ -29,66 +29,37 @@
         }
 
 
-        private  function setJwtAuthCode()
-        {
-            // Configures the time that the token was issue (iat claim)
-            $token = (new Builder())->setIssuedAt(time())
-                    ->setNotBefore(time() + 60)
-                    ->setExpiration(time() + 3600)
-                    ->set('uid', 1)
-                    ->set('nickname','maccha')
-                    ->getToken();
-
-            $data = [
-                'Authorization' => (string) $token,
-                'iat'           => $token->getClaim('iat'),
-                'nbf'           => $token->getClaim('nbf'),
-                'exp'           => $token->getClaim('exp'),
-            ];
-
-            return json([
-                'code'  => config('httpCode.OPTION_SUECCESS'),
-                'msg'   => 'SUECCESS',
-                'data'  => array_merge([
-                    'nickname'=>  'maccha',
-                    'openid'  =>  'yafd909asxqsxsxxasxgga86sd6v',
-                    'head_pic'=>  3123123
-                ],$data)
-            ],config('httpCode.OPTION_SUECCESS'), [])->send();
-        }
-
-
         private function WechatUserScope()
         {
             if( request()->has('code')
-                && strlen( request()->get('code')) > 0)
+                && strlen( request()->post('code')) > 0)
             {
-                $oauth = $this->app->oauth;
-                $user = $oauth->user();
-                $userinfo = $user->getOriginal();
-
-                file_put_contents('1.txt','-------'.PHP_EOL);
-                file_put_contents('1.txt',var_export($userinfo,true));
-                file_put_contents('1.txt','-------'.PHP_EOL);
-
-
-                //$user->getId();  // 对应微信的 OPENID
-//                $user->getNickname(); // 对应微信的 nickname
-//                $user->getName(); // 对应微信的 nickname
-//                $user->getAvatar(); // 头像网址
-//                $user->getOriginal(); // 原始API返回的结果
-//                $user->getToken(); // access_token， 比如用于地址共享时使用
-
+                 $this->UserScope();
             }else{
                 return $this->app->oauth->redirect()->send();
             }
         }
 
 
-        private function get_user_info()
+        protected function UserScope()
         {
+            $oauth = $this->app->oauth;
+            $user = $oauth->user();
+            $userinfo = $user->getOriginal();
 
+            file_put_contents('1.txt','-------'.PHP_EOL);
+            file_put_contents('1.txt',var_export($userinfo,true));
+            file_put_contents('1.txt','-------'.PHP_EOL);
+
+
+            //$user->getId();  // 对应微信的 OPENID
+//                $user->getNickname(); // 对应微信的 nickname
+//                $user->getName(); // 对应微信的 nickname
+//                $user->getAvatar(); // 头像网址
+//                $user->getOriginal(); // 原始API返回的结果
+//                $user->getToken(); // access_token， 比如用于地址共享时使用
         }
+
 
         protected function loginOrReg($openid)
         {
@@ -132,6 +103,35 @@
                     ]);
                 }
             }
-            return $user;
+            return $this->setJwtAuthCode($user);
+        }
+
+
+        private  function setJwtAuthCode($user)
+        {
+            // Configures the time that the token was issue (iat claim)
+            $token = (new Builder())->setIssuedAt(time())
+                ->setNotBefore(time() + 60)
+                ->setExpiration(time() + 3600)
+                ->set('id', $user['user_id'])
+                ->set('nickname',$user['nickname'])
+                ->getToken();
+
+            $data = [
+                'Authorization' => (string) $token,
+                'iat'           => $token->getClaim('iat'),
+                'nbf'           => $token->getClaim('nbf'),
+                'exp'           => $token->getClaim('exp'),
+            ];
+
+            return json([
+                'code'  => config('httpCode.OPTION_SUECCESS'),
+                'msg'   => 'SUECCESS',
+                'data'  => array_merge([
+                    'nickname'=>  $user['nickname'],
+                    'openid'  =>  $user['openid'],
+                    'head_pic'=>  $user['head_pic']
+                ],$data)
+            ],config('httpCode.OPTION_SUECCESS'), [])->send();
         }
     }
