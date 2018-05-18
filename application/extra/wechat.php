ca5899f187b9260a0fc193318ba43cac2ef4dd2a
[macccha520] <[1121787590@qq.com]>
2018-05-18 18:54:56 +0800

    update 312312

--- application/extra/wechat.php
+++ application/extra/wechat.php
@@ -60,7 +60,7 @@
             // 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
             'response_type' => 'array',
             'oauth' => [
-                'scopes'   => ['snsapi_userinfo'],
+                'scopes'   => ['snsapi_userbase'],
                 'callback' => 'http://m.hanxvc.com',
             ],
             'log' => [
